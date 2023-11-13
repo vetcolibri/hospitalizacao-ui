0ea1b20d6931ab178257c26b236266da5fe674fb
shinobi <josan5368@gmail.com>
2023-11-13 15:40:51 +0100

    refactor: renomeada as importações dos serviços.

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,23 @@ import router from './router'
 import { createPinia } from 'pinia'
 
 import { AxiosAdapter } from './lib/adapters/axios_adapter'
-import { MeasurementServiceAPI } from './services/measurement_service'
-import { AlertServiceAPI } from './services/alert_service'
+import { RoundService } from './services/round_service'
+import { AlertService } from './services/alert_service'
 import { Provided } from './lib/provided'
-import { PatientServiceAPI } from './services/patient_service'
+import { PatientService } from './services/patient_service'
 
 const pinia = createPinia()
 const app = createApp(App)
 
-const baseUrl = 'http://localhost:8000'
+const baseUrl = 'http://192.168.10.69:8000'
 const axiosAdapter = new AxiosAdapter()
-const patientService = new PatientServiceAPI(axiosAdapter, baseUrl)
-const measurementService = new MeasurementServiceAPI(axiosAdapter, baseUrl)
-const alertService = new AlertServiceAPI(axiosAdapter, baseUrl)
-const webSocket = new WebSocket('ws://localhost:8000/alerts/notifications')
+const patientService = new PatientService(axiosAdapter, baseUrl)
+const roundService = new RoundService(axiosAdapter, baseUrl)
+const alertService = new AlertService(axiosAdapter, baseUrl)
+const webSocket = new WebSocket('ws://192.168.10.69:8000/alerts/notifications')
 
 app.provide(Provided.PATIENT_SERVICE, patientService)
-app.provide(Provided.MEASUREMENT_SERVICE, measurementService)
+app.provide(Provided.ROUND_SERVICE, roundService)
 app.provide(Provided.ALERT_SERVICE, alertService)
 app.provide(Provided.WEBSOCKET, webSocket)
 
