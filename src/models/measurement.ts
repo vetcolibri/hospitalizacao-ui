1248d02c3c0036b6c4b4ebb72f8f439ab9f0977f
shinobi <josan5368@gmail.com>
2023-11-13 15:43:31 +0100

    refactor: alterada o nome da propriedades.

--- src/models/measurement.ts
+++ src/models/measurement.ts
@@ -1,7 +1,6 @@
 export interface Measurement {
     readonly name: string
     readonly value: string
-    readonly date?: string
-    readonly issudeAt?: string
+    readonly issuedAt: string
     readonly message?: string
 }
