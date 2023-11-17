421cceeb77aa93f176ff7b658e68fd8e2f83e656
shinobi <josan5368@gmail.com>
2023-11-17 17:16:06 +0100

    refactor: sex 17 nov 2023 17:16:06 WAT

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -15,23 +15,29 @@ interface Emits {
 const props = defineProps<Props>()
 const emits = defineEmits<Emits>()
 
-const query = ref<string>('')
-const showOptions = ref<boolean>(false)
-const selectedOptions = ref<string[]>([])
 const inputRef = ref<HTMLInputElement>()
+const selected = ref<string[]>([])
+const show = ref<boolean>(false)
+const query = ref<string>('')
 const optionsRef = ref()
 
-function toggleOptions() {
-    showOptions.value = !showOptions.value
+const search = computed(() => {
+    return props.options.filter((option) =>
+        option.toLowerCase().includes(query.value.toLowerCase())
+    )
+})
+
+function toggle() {
+    show.value = !show.value
 }
 
-function selectOption(idx: number) {
-    const option = searchOptions.value[idx]
+function select(idx: number) {
+    const option = search.value[idx]
 
-    if (selectedOptions.value.includes(option)) {
-        selectedOptions.value = selectedOptions.value.filter((selected) => selected !== option)
-        emits('update:modelValue', selectedOptions.value)
-        if (selectedOptions.value.length === 0) {
+    if (selected.value.includes(option)) {
+        selected.value = selected.value.filter((selected) => selected !== option)
+        emits('update:modelValue', selected.value)
+        if (selected.value.length === 0) {
             inputRef.value!.value = ''
         }
         return
@@ -39,47 +45,41 @@ function selectOption(idx: number) {
 
     if (props.modelValue.length === props.limit) return
 
-    inputRef.value!.value = '1'
-    selectedOptions.value.push(option)
-    emits('update:modelValue', selectedOptions.value)
+    inputRef.value!.value = '  '
+    selected.value.push(option)
+    emits('update:modelValue', selected.value)
 }
 
 function hasSelected(idx: number) {
-    return selectedOptions.value.includes(searchOptions.value[idx])
+    return selected.value.includes(search.value[idx])
 }
 
-function cleanSelectedOptions() {
-    selectedOptions.value = []
+function cleanSelected() {
+    selected.value = []
     inputRef.value!.value = ''
-    emits('update:modelValue', selectedOptions.value)
+    emits('update:modelValue', selected.value)
 }
 
-function hiddenOptions(event: Event) {
+function hidden(event: Event) {
     if (optionsRef.value && !optionsRef.value.contains(event.target)) {
-        showOptions.value = false
+        show.value = false
     }
 }
 
-const searchOptions = computed(() => {
-    return props.options.filter((option) =>
-        option.toLowerCase().includes(query.value.toLowerCase())
-    )
-})
-
 onMounted(async () => {
-    document.addEventListener('click', hiddenOptions)
+    document.addEventListener('click', hidden)
 })
 </script>
 <template>
     <div ref="optionsRef" class="relative">
         <div
-            class="cursor-pointer z-20 absolute w-full h-full top-0 left-0 bg-transparent p-2"
-            @click="toggleOptions()"
+            class="w-full h-full absolute top-0 left-0 p-2 z-20 bg-transparent cursor-pointer"
+            @click="toggle()"
         >
-            <span v-if="modelValue.length > 5">
+            <span v-if="modelValue.length > 3">
                 {{ `(${modelValue.length})` }} opções escolhidas
             </span>
-            <div class="" v-if="modelValue.length > 0 && modelValue.length <= 5">
+            <div class="" v-if="modelValue.length > 0 && modelValue.length <= 3">
                 <span v-for="option in modelValue" class="badge badge-dark mr-2">
                     {{ option }}
                 </span>
@@ -87,29 +87,27 @@ onMounted(async () => {
         </div>
         <input ref="inputRef" type="text" class="form-control z-10" required :placeholder="title" />
         <div
-            v-if="showOptions"
+            v-if="show"
             class="w-full h-48 absolute border rounded shadow-sm mt-2 p-2.5 border-gray-300 bg-white overflow-y-auto z-50 text-sm text-gray-500"
         >
             <input type="text" class="form-control mb-2" placeholder="Pesquisar" v-model="query" />
             <div v-if="modelValue.length > 0" class="flex justify-between">
                 <span>São permitidas no máximo {{ limit }} opções</span>
-                <div class="space-x-1 cursor-pointer" @click="cleanSelectedOptions()">
+                <div class="space-x-1 cursor-pointer" @click="cleanSelected()">
                     <i class="bi bi-trash"></i>
                     <span>Limpar</span>
                 </div>
             </div>
             <ul class="mt-2">
                 <li
-                    v-for="(option, idx) in searchOptions"
+                    v-for="(option, idx) in search"
                     class="flex items-center gap-2 border-t p-2.5 cursor-pointer hover:bg-gray-50"
-                    @click="selectOption(idx)"
+                    @click="select(idx)"
                 >
                     <input type="checkbox" :checked="hasSelected(idx)" />
                     <span>{{ option }}</span>
                 </li>
-                <li class="p-2.5" v-if="searchOptions.length === 0">
-                    Nenhum resultado encontrado.
-                </li>
+                <li v-if="search.length === 0" class="p-2.5">Nenhum resultado encontrado.</li>
             </ul>
         </div>
     </div>
